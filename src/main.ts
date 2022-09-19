6132d1eb90d5964a6e23667274b1d6c8b1416e0c
“青菜白玉汤” <2075125282@qq.com>
2022-09-19 17:04:02 +0800

    :zap: 重构vue挂载逻辑

--- src/main.ts
+++ src/main.ts
@@ -1,32 +1,20 @@
 import { createApp } from "vue";
-
 import App from "./App.vue";
-import router, { setupRouter } from "./router";
+import { setupRouter } from "./router";
 import "./assets/css/taiwind.css";
 import { setupStore } from "@/stores";
-import { AppProvider } from "@/components/Application";
 
-async function bootstrap() {
-  const appProvider = createApp(AppProvider);
-
+async function setupApp() {
   const app = createApp(App);
 
-  // 挂载状态管理
+  // 安装pinia
   setupStore(app);
 
-  //优先挂载一下 Provider 解决路由守卫，Axios中可使用，Dialog，Message 等之类组件
-  appProvider.mount("#appProvider", true);
-
-  // 挂载路由
+  // 安装路由
   await setupRouter(app);
 
-  // 路由准备就绪后挂载APP实例
-  await router.isReady();
-
-  app.mount("#app", true);
+  // mount app
+  app.mount("#app");
 }
 
-const meta = document.createElement("meta");
-meta.name = "naive-ui-style";
-document.head.appendChild(meta);
-void bootstrap();
+setupApp();
