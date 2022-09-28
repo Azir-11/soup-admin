6c0ff6c05744c5f6ec0d268166540cba27ef5d9e
“青菜白玉汤” <2075125282@qq.com>
2022-09-28 15:29:32 +0800

    :recycle: 修改静态css、js的引入方式

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
 import { createApp } from "vue";
 import App from "./App.vue";
-import 'uno.css';
 import { setupRouter } from "./router";
 import { setupStore } from "@/stores";
+import { setupAssets } from "./plugins";
 
 async function setupApp() {
+  // import assets: js、css
+  setupAssets();
+
   const app = createApp(App);
 
   // 安装pinia
