7c46d9dfd55da01a0bf2c63a8ea9b034a1937690
“青菜白玉汤” <2075125282@qq.com>
2022-11-28 16:13:12 +0800

    :sparkles: 封装按钮hover组件，废弃不美观的用类名控制的方式

--- uno.config.ts
+++ uno.config.ts
@@ -20,8 +20,6 @@ export default defineConfig({
     "flex-x-center": "flex justify-center",
     "flex-y-center": "flex items-center",
     "flex-1-hidden": "flex-1 overflow-hidden",
-    hoverBtn:
-      "flex h-full w-14 cursor-pointer items-center justify-center duration-300 hover:bg-gray-200",
     globalComponents: "fixed transition-all",
   },
   theme: {},
