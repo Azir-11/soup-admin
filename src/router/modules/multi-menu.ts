42915eb1a9d46934b9f27cfecc8182ea8296fe43
“青菜白玉汤” <2075125282@qq.com>
2022-12-29 16:16:22 +0800

    :alien: 删除旧的路由信息提示

--- src/router/modules/multi-menu.ts
+++ src/router/modules/multi-menu.ts
@@ -1,16 +1,3 @@
-/**
- * @param name 路由名称, 必须设置,且不能重名
- * @param meta 路由元信息（路由附带扩展信息）
- * @param redirect 重定向地址, 访问这个路由时,自定进行重定向
- * @param meta.disabled 禁用整个菜单
- * @param meta.title 菜单名称
- * @param meta.icon 菜单图标
- * @param meta.keepAlive 缓存该路由
- * @param meta.sort 排序越小越排前
- * @param meta.hidden 隐藏这个菜单项
- * @param meta.tabsHidden 在标签页中不显示该路由
- * */
-
 const multiMenu: AuthRoute.Route = {
   name: "multi-menu",
   path: "/multi-menu",
